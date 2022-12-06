c170093951834b8c549247ecc74d94a4589efd89
liang <liang.liu@china-dongcheng.com>
2022-12-06 20:33:34 +0800

    feat: 权限申请

--- lib/page/config/prepare/prepare_page.dart
+++ lib/page/config/prepare/prepare_page.dart
@@ -1,5 +1,8 @@
+import 'package:diagnosis_tool/common/text_style.dart';
+import 'package:diagnosis_tool/page/config/prepare/prepare_provider.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 class PreparePage extends ConsumerStatefulWidget {
@@ -23,6 +26,88 @@ class PrepareConsumerState extends ConsumerState {
       appBar: AppBar(
         elevation: 0,
         title: const Text('添加准备'),
+      ),
+      body: Container(
+        padding: const EdgeInsets.all(16),
+        child: Column(
+          children: [
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(16, 30, 0, 0),
+                child: Text(
+                  "1、请确保机器人处于开机状态",
+                  style: commonStyle(),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.centerLeft,
+              child: Container(
+                margin: const EdgeInsets.fromLTRB(16, 12, 0, 0),
+                child: Text(
+                  "2、请长按回充按键6s，等待听到“进入配网模式”",
+                  style: commonStyle(),
+                  textAlign: TextAlign.start,
+                ),
+              ),
+            ),
+            const Expanded(child: SizedBox()),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                Checkbox(
+                  value: ref
+                      .watch(prepareProvider.select((value) => value.checked)),
+                  onChanged: (value) {
+                    ref.read(prepareProvider.notifier).updateChecked(value!);
+                  },
+                  shape: const CircleBorder(),
+                  activeColor: const Color(0XFF274DFD),
+                ),
+                Text(
+                  "已听到“进入配网模式”",
+                  style: commonStyle(),
+                  textAlign: TextAlign.center,
+                )
+              ],
+            ),
+            Padding(
+                padding: const EdgeInsets.fromLTRB(0, 0, 0, 20),
+                child: TextButton(
+                  onPressed: () {
+                    if (!ref.watch(
+                        prepareProvider.select((value) => value.checked))) {
+                      print('checked false');
+                      return;
+                    }
+
+                    print('checked true');
+
+                    ref
+                        .read(prepareProvider.notifier)
+                        .isPermissionGranted()
+                        .then((value) {
+                      if (value) {
+                        context.push('/scan');
+                      } else {
+                        ref.read(prepareProvider.notifier).requestPermissions();
+                      }
+                    });
+                  },
+                  style: ButtonStyle(
+                    backgroundColor:
+                        MaterialStateProperty.all(Color(0xFF353547)),
+                    foregroundColor: MaterialStateProperty.all(Colors.white),
+                    shape: MaterialStateProperty.all(RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(12))),
+                    fixedSize: MaterialStateProperty.all(Size(343, 66)),
+                  ),
+                  child: const Text("开始添加"),
+                )),
+          ],
+        ),
       ),
     );
   }
--- lib/page/config/prepare/prepare_provider.dart
+++ lib/page/config/prepare/prepare_provider.dart
@@ -1,49 +1,98 @@
 import 'dart:io';
 
 import 'package:device_info_plus/device_info_plus.dart';
+import 'package:freezed_annotation/freezed_annotation.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 import 'package:permission_handler/permission_handler.dart';
 
-final prepareProvider = StateNotifierProvider((ref) => PrepareStateNotifier());
+part 'prepare_provider.freezed.dart';
 
-class PrepareStateNotifier extends StateNotifier {
-  PrepareStateNotifier() : super(null);
+part 'prepare_provider.g.dart';
+
+@freezed
+class PrepareState with _$PrepareState {
+  const factory PrepareState({
+    required bool checked,
+    required bool permissionGranted,
+  }) = _PrepareState;
+
+  factory PrepareState.fromJson(Map<String, dynamic> json) =>
+      _$PrepareStateFromJson(json);
+}
+
+final prepareProvider =
+    StateNotifierProvider.autoDispose<PrepareStateNotifier, PrepareState>(
+        (ref) => PrepareStateNotifier());
+
+class PrepareStateNotifier extends StateNotifier<PrepareState> {
+  PrepareStateNotifier()
+      : super(const PrepareState(checked: false, permissionGranted: false));
+
+  Future<bool> isPermissionGranted() async {
+    if (Platform.isAndroid) {
+      DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
+      var androidInfo = await deviceInfo.androidInfo;
+      if (androidInfo.version.sdkInt >= 31) {
+        if (await Permission.bluetoothConnect.status.isGranted &&
+            await Permission.bluetoothScan.status.isGranted) {
+          return true;
+        } else {
+          return false;
+        }
+      } else {
+        if (await Permission.locationWhenInUse.status.isGranted &&
+            await Permission.bluetoothAdvertise.status.isGranted &&
+            await Permission.bluetoothScan.status.isGranted &&
+            await Permission.bluetooth.status.isGranted &&
+            await Permission.bluetoothConnect.status.isGranted) {
+          return true;
+        } else {
+          return false;
+        }
+      }
+    } else if (Platform.isIOS) {
+      final status = await Permission.locationWhenInUse.status;
+      return status.isGranted;
+    } else {
+      return false;
+    }
+  }
 
   // 申请蓝牙权限
   // You can request multiple permissions at once.
-  Future<bool> requestPermissions() async {
+  void requestPermissions() async {
     // 如果是Android平台
+    var granted = false;
     if (Platform.isAndroid) {
-      return await _requestAndroidPermissions();
+      granted = await _requestAndroidPermissions();
     } else if (Platform.isIOS) {
-      return await _requestIOSPermissions();
+      granted = await _requestIOSPermissions();
     }
-    return false;
+
+    state = state.copyWith(permissionGranted: granted);
   }
 
   // 申请Android蓝牙权限
   Future<bool> _requestAndroidPermissions() async {
     // 如果sdk版本大于等于29
     DeviceInfoPlugin deviceInfo = DeviceInfoPlugin();
-    var status;
-    if (Platform.isAndroid) {
-      var androidInfo = await deviceInfo.androidInfo;
-      if (androidInfo.version.sdkInt >= 31) {
-        status = await [
-          Permission.bluetoothConnect,
-          Permission.bluetoothScan,
-        ].request();
-      } else {
-        status = await [
-          Permission.locationWhenInUse,
-          Permission.bluetooth,
-          Permission.bluetoothConnect,
-          Permission.bluetoothScan,
-          Permission.bluetoothAdvertise,
-        ].request();
-      }
+    Map<Permission, PermissionStatus> status;
+    var androidInfo = await deviceInfo.androidInfo;
+    if (androidInfo.version.sdkInt >= 31) {
+      status = await [
+        Permission.bluetoothConnect,
+        Permission.bluetoothScan,
+      ].request();
+    } else {
+      status = await [
+        Permission.locationWhenInUse,
+        Permission.bluetooth,
+        Permission.bluetoothConnect,
+        Permission.bluetoothScan,
+        Permission.bluetoothAdvertise,
+      ].request();
     }
-    return status.isGranted;
+    return status.values.every((element) => element.isGranted);
   }
 
   // 申请iOS蓝牙权限
@@ -52,5 +101,7 @@ class PrepareStateNotifier extends StateNotifier {
     return status.isGranted;
   }
 
-  void scan() {}
+  void updateChecked(bool checked) {
+    state = state.copyWith(checked: checked);
+  }
 }
